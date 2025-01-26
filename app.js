c5565719f4c75d7f2ecc5e98493835bc9e2e4c1c
libra625 <18.12.17.moyo@gmail.com>
2025-01-27 00:19:04 +0200

    done

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
-const fs = require('fs');
+const express = require('express')
+const app = express()
+const port = 3000
 
-fs.readFile('data.txt', 'utf8', (err, data) => {
-    if (err) {
-        console.error("Помилка читання файлу:", err);
-        return;
-    }
-    console.log("Вміст файлу data.txt:", data);
+app.use(express.json());
+
+app.use((req, res, next) => {
+    console.log(`REQ: ${req.method} URL: ${req.url}`);
+    next();
 });
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/user/:id', (req, res) => {
+    const id = req.params.id
+    res.send(`Користувач з ID: ${id}`)
+})
+
+app.post('/submit', (req, res) => {
+    const {name, email} = req.body
+    res.send(`Отримано дані: Ім'я - ${name}, Електронна пошта - ${email}`);
+});
+
+app.use((req, res) => {
+    // console.log(err.message)
+    // console.log(err.status)
+    res.status(404).send('Сторінка не знайдена');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
